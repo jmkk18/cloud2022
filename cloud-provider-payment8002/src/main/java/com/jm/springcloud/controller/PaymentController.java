0174a7c23ca11ccb8565180fba5cf3821790f59e
jm <jm@qq.com>
2022-11-22 18:55:44 +0800

    jm 1

---
+++ cloud-provider-payment8002/src/main/java/com/jm/springcloud/controller/PaymentController.java
@@ -0,0 +1,44 @@
+package com.jm.springcloud.controller;
+
+import com.jm.springcloud.entities.CommonResult;
+import com.jm.springcloud.entities.Payment;
+import com.jm.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+public class PaymentController {
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @PostMapping("/payment/create")
+    public CommonResult create(@RequestBody Payment payment) {
+        int result = paymentService.create(payment);
+        log.info("*****插入结果：" + result);
+
+        if (result > 0) {
+            return new CommonResult(200, "消息插入数据库成功,serverPort:"+serverPort, result);
+        } else {
+            return new CommonResult(444, "消息插入数据库失败", null);
+        }
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult getPayment(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*****查询结果：" + payment);
+
+        if (payment != null) {
+            return new CommonResult(200, "消息查询成功,serverPort:"+serverPort, payment);
+        } else {
+            return new CommonResult(444, "没有对应记录,查询ID：" + id, null);
+        }
+    }
+}
---
+++ cloud-provider-payment8002/src/main/java/com/jm/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.jm.springcloud.service.impl;
+
+import com.jm.springcloud.dao.PaymentDao;
+import com.jm.springcloud.entities.Payment;
+import com.jm.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
